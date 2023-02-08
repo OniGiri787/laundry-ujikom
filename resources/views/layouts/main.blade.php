c0f0ae26b5b9849af139e10e10d1a3569ef32bb6
zaazxz <mirzaqamaruzzaman18@gmail.com>
2023-02-08 14:05:38 +0700

    komit pertama

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+
+    <link rel="stylesheet" href="{{ asset('assets/css/main/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/main/app-dark.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/shared/iconly.css') }}">
+
+    {{-- Icon --}}
+    <link rel="shortcut icon" href="assets/images/logo/favicon.svg" type="image/x-icon">
+    <link rel="shortcut icon" href="assets/images/logo/favicon.png" type="image/png">
+
+</head>
+
+<body>
+    <div id="app">
+
+        {{-- Sidebar --}}
+        @include('layouts.partials.sidebar')
+
+        {{-- Content --}}
+        <div id="main">
+
+            <header class="mb-3">
+                <a href="#" class="burger-btn d-block d-xl-none">
+                    <i class="bi bi-justify fs-3"></i>
+                </a>
+            </header>
+
+            <div class="page-heading">
+                <div class="row">
+                    <div class="col-md-8 pt-5">
+                        <h3>Profile Statistics</h3> 
+                    </div>
+                    <div class="col-md-4 pt-2">
+                        
+                    </div>
+                </div>
+            </div>
+
+            {{-- Penghubung Content --}}
+            @yield('content')
+
+            {{-- Footer --}}
+            @include('layouts.partials.footer')
+
+        </div>
+
+    </div>
+    <script src="assets/js/bootstrap.js"></script>
+    <script src="assets/js/app.js"></script>
+
+    <!-- Need: Apexcharts -->
+    <script src="assets/extensions/apexcharts/apexcharts.min.js"></script>
+    <script src="assets/js/pages/dashboard.js"></script>
+
+</body>
+
+</html>
